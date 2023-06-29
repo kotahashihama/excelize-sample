5de44ceb8456d0d89d63868e46e9b0fcd3cdc3e4
Kota Hashihama <kotahashihama@gmail.com>
2023-06-29 16:28:38 +0900

    feat(main.go): マスターファイルの作成関数を追加

--- main.go
+++ main.go
@@ -5,17 +5,79 @@ import (
 	"log"
 	"os"
 
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+
 	"github.com/joho/godotenv"
 	"github.com/xuri/excelize/v2"
 )
 
+const sheet = "Sheet1"
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal(err)
 	}
 
-	createHelloWorldExcelFile()
+	createMasterExcelFile()
+	// createHelloWorldExcelFile()
+}
+
+func createMasterExcelFile() {
+	f := excelize.NewFile()
+	desktopPath := os.Getenv("ABSOLUTE_PATH_TO_DESKTOP")
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}()
+
+	// 見出し
+	f.SetCellValue(sheet, "A1", "店舗名")
+	f.SetCellValue(sheet, "B1", "完了日時")
+	f.SetCellValue(sheet, "C1", "更新日時")
+	f.SetCellValue(sheet, "D1", "報告者")
+	f.SetCellValue(sheet, "E1", "テキスト1")
+	f.SetCellValue(sheet, "F1", "テキスト2")
+	f.SetCellValue(sheet, "G1", "画像1")
+	f.SetCellValue(sheet, "H1", "画像2")
+	f.SetCellValue(sheet, "I1", "画像3")
+
+	// 内容
+	f.SetCellValue(sheet, "A2", "hoge")
+	f.SetCellValue(sheet, "B2", "hoge")
+	f.SetCellValue(sheet, "C2", "hoge")
+	f.SetCellValue(sheet, "D2", "hoge")
+	f.SetCellValue(sheet, "E2", "hoge")
+	f.SetCellValue(sheet, "F2", "hoge")
+	if err := f.AddPicture(sheet, "G2", fmt.Sprintf("%s/image.png", desktopPath), &excelize.GraphicOptions{
+		AutoFit: true,
+	}); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := f.AddPicture(sheet, "H2", fmt.Sprintf("%s/image.png", desktopPath), &excelize.GraphicOptions{
+		AutoFit: true,
+	}); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := f.AddPicture(sheet, "I2", fmt.Sprintf("%s/image.png", desktopPath), &excelize.GraphicOptions{
+		AutoFit: true,
+	}); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// ファイルを保存
+	if err := f.SaveAs(fmt.Sprintf("%s/master.xlsx", desktopPath)); err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
 func createHelloWorldExcelFile() {
